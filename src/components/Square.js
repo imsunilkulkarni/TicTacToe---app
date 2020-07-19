1636ba5aa3cfc33f42992e54a94f77bf53bdd899
imsunilkulkarni <sunilkulkarnink@gmail.com>
2020-07-19 20:30:16 +0530

    Add files via upload

---
+++ src/components/Square.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../App.css'
+
+const Square = (props) => {
+    return(
+        <div>
+            <button 
+            className="square" 
+                    onClick={props.onClick}>
+                    {props.value}
+            </button>
+        </div>
+    )
+}
+
+ 
+export default Square
